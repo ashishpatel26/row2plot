5affe399c3de3edbc4135f3519d58124fd66d777
Ashish Patel <shriganesh.patel@gmail.com>
2020-05-17 18:39:21 +0530

    tar update

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name='row2plot',
-    version='1.0',
-    packages=['pandas','matplotlib','seaborn','tqdm'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-    author="Ashish Patel"
-)
-
-setup(
     name = 'row2plot',
-    install_requires=['pandas','matplotlib','seaborn','tqdm']
-    version = 'version number',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
-    author = '',
-    author_email = '',
-    url = 'github package source url',
+    install_requires=['pandas','matplotlib','seaborn','tqdm'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'This package is use for convert your row to histogram and kdeplot',
+    author = 'Ashish Patel',
+    author_email = 'ashishpatel26',
+    url = 'https://github.com/ashishpatel26/row2plot',
     download_url = 'download link you saved',
-    keywords = ['tag1', 'tag2'],
+    keywords = ['pandas', 'dataset','matplotlib','seaborn','row2plot'],
     classifiers = [],
+    long_description=open('README.txt').read(),
 )
