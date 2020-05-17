514f095db4ccb3812900e691b36139842f486810
Ashish Patel <shriganesh.patel@gmail.com>
2020-05-17 18:40:54 +0530

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version = '1.0',  # Ideally should be same as your GitHub release tag varsion
     description = 'This package is use for convert your row to histogram and kdeplot',
     author = 'Ashish Patel',
-    author_email = 'ashishpatel26',
+    author_email = 'ashishpatel.ce.2011@gmail.com',
     url = 'https://github.com/ashishpatel26/row2plot',
     download_url = 'download link you saved',
     keywords = ['pandas', 'dataset','matplotlib','seaborn','row2plot'],
